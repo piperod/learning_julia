b6838a070351b2c490e8f8d7f921aec59c4f0186
Felipe Rodriguez <ifelipe.rodriguez@hotmail.com>
2016-05-09 20:03:52 -0400

    profiling

--- Presentation/profileparallel.jl
+++ Presentation/profileparallel.jl
@@ -13,3 +13,19 @@ Alloc=@allocated Deltastep(graph,5000,i)
 write(f,"Image :$size  Deltastep : $i Time Elapsed $Time Memory Allocations : $Alloc \n ")
 end
 close(f)
+img=load("Imagenes/escudo.jpeg")
+f=open("outputescudo.txt","w")
+for i in [10,50,100,500,1000]
+Time=@elapsed Deltastep(graph,400,i)
+Alloc=@allocated Deltastep(graph,400,i)
+write(f,"  Deltastep : $i Time Elapsed $Time Memory Allocations : $Alloc \n")
+end
+close(f)
+img=load("Imagenes/Julia.png")
+f=open("outputjulia.txt","w")
+for i in [10,50,100,500,1000]
+Time=@elapsed Deltastep(graph,400,i)
+Alloc=@allocated Deltastep(graph,400,i)
+write(f,"  Deltastep : $i Time Elapsed $Time Memory Allocations : $Alloc \n")
+end
+close(f)
--- Presentation/profileserial.jl
+++ Presentation/profileserial.jl
@@ -20,9 +20,20 @@ Time=@elapsed Deltastep(graph,5000,i)
 Alloc=@allocated Deltastep(graph,5000,i)
 write(f,"  Deltastep : $i Time Elapsed $Time Memory Allocations : $Alloc \n")
 end
-
-img=load("rose.png")
-
-
-
+close(f)
+img=load("Imagenes/escudo.jpeg")
+f=open("outputescudo.txt","w")
+for i in [10,50,100,500,1000]
+Time=@elapsed Deltastep(graph,400,i)
+Alloc=@allocated Deltastep(graph,400,i)
+write(f,"  Deltastep : $i Time Elapsed $Time Memory Allocations : $Alloc \n")
+end
+close(f)
+img=load("Imagenes/Julia.png")
+f=open("outputjulia.txt","w")
+for i in [10,50,100,500,1000]
+Time=@elapsed Deltastep(graph,400,i)
+Alloc=@allocated Deltastep(graph,400,i)
+write(f,"  Deltastep : $i Time Elapsed $Time Memory Allocations : $Alloc \n")
+end
 close(f)
